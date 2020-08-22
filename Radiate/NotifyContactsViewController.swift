406991f4cf08ea15cf175ba1130d903759ee4d87
Divya <divya_karivaradasamy@intuit.com>
2020-08-22 15:18:28 -0700

    Version 2

---
+++ Radiate/NotifyContactsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NotifyContactsViewController.swift
+//  Radiate
+//
+//  Created by dkarivarad on 8/22/20.
+//  Copyright © 2020 TeamRadiate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotifyContactsViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
